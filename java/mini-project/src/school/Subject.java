e33ebcebbe536e259b097558bdcdd2a61043737f
songstone <thdtmdgus5505@gmail.com>
2021-09-25 15:35:27 +0900

    feat: implement Subject class
    - define member variable
    - define constructor
    - define getter and setter

--- java/mini-project/src/school/Subject.java
+++ java/mini-project/src/school/Subject.java
@@ -1,4 +1,42 @@
 package school;
 
+import java.util.ArrayList;
+
 public class Subject {
+
+    private static int seq = 0;
+
+    private String subjectID;
+    private String subjectName;
+    private ArrayList<Student> studentList;
+
+    public Subject(String subjectName){
+        this.subjectName = subjectName;
+        this.subjectID = String.format("%04d",seq);
+        studentList = new ArrayList<>();
+    }
+
+    public String getSubjectID() {
+        return subjectID;
+    }
+
+    public void setSubjectID(String subjectID) {
+        this.subjectID = subjectID;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
 }
