211bacccdeb0e3f2a15eee5da742b2399b2a30da
songstone <thdtmdgus5505@gmail.com>
2021-09-25 15:21:27 +0900

    feat: implement Score class
    - define member varialbe
    - define getter and setter
    - define constructor

--- java/mini-project/src/school/Score.java
+++ java/mini-project/src/school/Score.java
@@ -1,4 +1,36 @@
 package school;
 
 public class Score {
+    private String studentID;
+    private Subject subject;
+    private int point;
+
+    public Score(Subject subject, int point){
+        this.subject = subject;
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
 }
