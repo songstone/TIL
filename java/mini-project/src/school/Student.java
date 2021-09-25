b66e4a27626eedb7ad14ae65d1314ee5ca4113e0
songstone <thdtmdgus5505@gmail.com>
2021-09-25 14:59:12 +0900

    feat: define Constructor
    - define Student's constructor

--- java/mini-project/src/school/Student.java
+++ java/mini-project/src/school/Student.java
@@ -1,4 +1,23 @@
 package school;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Student {
+
+    private static int seq = 0;
+    private static Calendar calendar = Calendar.getInstance();
+
+    private String studentID;
+    private String studentName;
+    private Subject major;
+    private ArrayList<Score> scoreList;
+
+    public Student(String studentName, Subject major){
+        this.studentID = String.format(calendar.get(Calendar.YEAR)+"%04d",++seq);
+        this.studentName = studentName;
+        this.major = major;
+        this.scoreList = new ArrayList<>();
+    }
+
 }
