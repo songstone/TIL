f72983d658097782a70949c091441403fe42cfef
songstone <thdtmdgus5505@gmail.com>
2021-09-26 15:20:17 +0900

    feat: implement makeHeader method
    - implement makeHeader method

--- java/mini-project/src/school/GenerateGradeReport.java
+++ java/mini-project/src/school/GenerateGradeReport.java
@@ -1,14 +1,44 @@
 package school;
 
+import java.sql.Struct;
+
 public class GenerateGradeReport {
     private StringBuffer report;
+    private School school;
 
     private static final String TITLE = " 과목 수강생 학점 \t\t\n";
     private static final String HEADER = " 이름  |  학번  |  전공  |  점수  \n";
-    private static final String LiNE = "-------------------------";
+    private static final String LINE = "-------------------------";
 
 
     public GenerateGradeReport(){
+        school = School.getInstance();
+    }
+
+    public void gradeReport(){
         report = new StringBuffer();
+        for(Subject subject : school.getSubjectList()){
+
+        }
+    }
+
+    public void gradeReport(Subject subject){
+
+    }
+
+    private void makeHeader(Subject subject){
+        report.append(LINE);
+        report.append(subject.getSubjectName());
+        report.append(TITLE);
+        report.append(HEADER);
+        report.append(LINE);
+    }
+
+    private void makeBody(Subject subject){
+
+    }
+
+    public void makeFooter(Subject subject){
+
     }
 }
