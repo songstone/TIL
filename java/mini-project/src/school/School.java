9f680166eb008db5202750fcc3392cf6fa0b2d1b
songstone <thdtmdgus5505@gmail.com>
2021-09-26 14:54:58 +0900

    feat: define methods about Lists
    - define getter method
    - define add to Lists method
    - define remove from Lists method

--- java/mini-project/src/school/School.java
+++ java/mini-project/src/school/School.java
@@ -17,5 +17,27 @@ public class School {
         return instance;
     }
 
+    public void addSubject(Subject subject){
+        subjectList.add(subject);
+    }
 
+    public void removeSubject(Subject subject){
+        subjectList.remove(subject);
+    }
+
+    public void addStudent(Student student){
+        studentList.add(student);
+    }
+
+    public void removeStudent(Student student){
+        studentList.remove(student);
+    }
+
+    public ArrayList<Subject> getSubjectList() {
+        return subjectList;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
 }
