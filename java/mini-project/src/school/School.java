97c8b5c7d62b1cd64ca51a910fb74bd18afdaa2d
songstone <thdtmdgus5505@gmail.com>
2021-09-26 14:51:24 +0900

    feat: define School with singleton pattern
    - define School with singleton pattern
    - define member variable

--- java/mini-project/src/school/School.java
+++ java/mini-project/src/school/School.java
@@ -1,4 +1,21 @@
 package school;
 
+import java.util.ArrayList;
+
 public class School {
+
+    private static School instance = new School();
+    private ArrayList<Subject> subjectList = new ArrayList<>();
+    private ArrayList<Student> studentList = new ArrayList<>();
+
+    private School(){}
+
+    public static School getInstance(){
+        if(instance == null){
+            instance = new School();
+        }
+        return instance;
+    }
+
+
 }
