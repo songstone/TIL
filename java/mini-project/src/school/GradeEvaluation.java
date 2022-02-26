4ca047516a481d3172d939c8f039e0e9bc12bf76
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-02-26 09:02:59 +0900

    modify folder hierarchy

--- java/mini-project/src/school/GradeEvaluation.java
+++
@@ -1,5 +0,0 @@
-package school;
-
-public interface GradeEvaluation {
-    public String getGrade(int point);
-}
--- python/python-prac/my_excel.py
+++
@@ -1,6 +0,0 @@
-class Excel:
-    def __init__(self):
-        self.excel_file = ''
-    
-    def save_to_excel(self, data):
-        print('Save (' +str(len(data))+')data : '+self.excel_file)
